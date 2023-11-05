b50c1c11ec536b21532d6a720f1eaa20ed917ad1
christianlepio <rylepio79@gmail.com>
2023-11-05 22:19:36 +0800

    update about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const About = () => {
     return (
-        <main>
-            <h1>About</h1>
+        <main className='mt-3'>
+            <h1 className='text-center'>About Page</h1>
         </main>
     )
 }
